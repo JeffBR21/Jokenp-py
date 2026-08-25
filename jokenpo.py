7fd13fbb2ae8a300d3bfd256c44a3b0662506b0d
Jefferson Dias <145174445+JeffBR21@users.noreply.github.com>
2026-08-25 23:46:21 +0000

    inicial

---
+++ jokenpo.py
@@ -0,0 +1,17 @@
+print("Olá, Jogador!")
+print("1 - Iniciar jogo")
+print("2 - sair")
+n = input(int("Escolha uma opção entre 1 e 2: "))
+
+while n:
+    if n == 1:
+        print("Escolha os numeros de 1 a 3")
+        print("1 - pedra")
+        print("2 - papel")
+        print("3 - tesoura")
+        n2 = input(int("Qual é a sua escolha: "))
+
+        print(f"Resultado {n2}")
+
+    
+         
